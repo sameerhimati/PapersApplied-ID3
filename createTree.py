1871b9b86050ca9f5c474b0a649934122e1e8ac2
Sameer Himati <sameerhimati98@gmail.com>
2025-02-15 00:37:23 -0600

    added info gain function

--- createTree.py
+++ createTree.py
@@ -1,4 +1,4 @@
-
+import math
 
 
 class Node:
@@ -10,12 +10,16 @@ class Node:
 
 
 class DecisionTree:
-    def __init__(self, input_df, features, target):
-        self.df = input_df
+    def __init__(self, x, features, target): # features = ['Outlook', 'Temperature', 'Humidity', 'Wind']; # target = ['Class']
+        self.x = x
         self.features = features
         self.target = target
         self.node = None
-        self.entropy = self._entropy([])
+        self.entropy = []
+        for i in range(len(self.target)): # calculate initial entropy
+            self.entropy.append(i) # list of numbers 0 to len(target)
+        
+        self.entropy = self._entropy(self.entropy) 
 
     def _entropy(self, values):
         """
@@ -27,11 +31,59 @@ class DecisionTree:
 
         Parameters
         ----------
-        values : list of str
+        values : list of ids
             The list of values to calculate the entropy of.
 
         Returns
         -------
         float
         """
-        pass
+        y = [self.target[i] for i in values] # get the target values (Yes or No) = ['Yes', 'Yes', 'No', 'Yes', ... etc]
+
+        target_values = [y.count(classes) for classes in set(y)] # count the number of times each class appears in the target values {'Yes': 510, 'No': 490}
+
+        if sum(target_values) == 0: # if all values are the same
+            return 0
+        
+        entropy = sum([-classes/len(y) * math.log(classes/len(y), 2) for classes in target_values]) # calculate the entropy based on the formula 
+
+        return entropy
+
+    def _information_gain(self, values, feature):
+        """
+        Calculates the information gain for a given feature.
+
+        Information gain is a measure of how much information can be gained when
+        a decision is made based on the values.
+
+        Parameters
+        ----------
+        values : list of ids
+            The list of values to calculate the information gain of.
+
+        Returns
+        -------
+        float
+        """
+        total_entropy = self._entropy(values)
+
+        feature_values = [self.x[i][feature] for i in values] # get the feature values (For example, Outlook) = ['Sunny', 'Overcast', 'Rain', 'Sunny', ... etc]
+
+        unique_values = list(set(feature_values)) # get the unique values ['Sunny', 'Overcast', 'Rain']
+
+        feature_entropy = []
+
+        for value in unique_values:
+            # Get indices where feature has this value
+            value_indices = [values[i] for i in range(len(feature_values)) if feature_values[i] == value] # A list of indices where the feature value is the same as the value, eg [0, 3, 4, 7...]
+            
+            # Calculate entropy for this subset
+            value_entropy = self._entropy(value_indices) # eg. Entropy for Sunny
+
+            # Get proportion of samples with this value
+            value_proportion = len(value_indices) / len(values) # how many Sunny/All Values
+            feature_entropy.append(value_entropy * value_proportion) # Sunny's Entropy added to the list
+
+        info_gain = total_entropy - sum(feature_entropy)
+
+        return info_gain
